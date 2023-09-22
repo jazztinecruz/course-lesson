c65404e5ceb16a1e981fb9cd9732f83dc952e1dc
Jazztine Cruz <jzztn.crz@gmail.com>
2023-09-22 16:22:00 +0800

    chore: move components outside app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
+import Navbar from "@/components/navbar";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Navbar from "./_components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
