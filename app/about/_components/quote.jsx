e22cbc46344834b727c21262fb06316d018f7da7
Jazztine Cruz <jzztn.crz@gmail.com>
2023-09-23 20:45:58 +0800

    chore: fix path

--- app/about/_components/quote.jsx
+++ app/about/_components/quote.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Quote = () => {
   return (
     <section className="flex flex-col items-center justify-center gap-4 text-center px-4 h-64">
-      <Image src="/images/quote.png" alt="Quote Icon" width={40} height={40} />
+      <Image src="/images/icons/quote.png" alt="Quote Icon" width={40} height={40} />
       <p className="max-w-2xl text-lg font-medium">
         I will not follow where the path may lead, but I will go where there is
         no path, and I will leave a trail.
