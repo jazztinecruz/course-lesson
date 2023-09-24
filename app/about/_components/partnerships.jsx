c9b7c4ececfe985ab3e87285e708a099d239e3cc
Jazztine Cruz <jzztn.crz@gmail.com>
2023-09-24 17:12:57 +0800

    chorE: fix partner fetching issue

--- app/about/_components/partnerships.jsx
+++ app/about/_components/partnerships.jsx
@@ -1,23 +1,36 @@
-import { PARTNERSHIPS } from "@/constants/about";
-import Image from "next/image";
+"use client";
 
-const Partnerships = async () => {
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+const Partnerships = () => {
+  const [partners, setPartners] = useState([]);
+
+  useEffect(() => {
+    const fetchPartners = async () => {
+      const response = await fetch("http://localhost:3001/partners");
+      const partners = await response.json();
+      setPartners(partners);
+    };
+    fetchPartners();
+  }, []);
+
   return (
     <section className="flex flex-col items-center gap-4">
       <h2 className="text-center text-2xl font-bold">Our Community Partners</h2>
 
       <ul className="grid md:grid-cols-2 gap-16 py-4 max-w-7xl">
-        {PARTNERSHIPS.map((partner) => (
+        {partners.map((partner) => (
           <div
-            key={partner.alt}
+            key={partner.id}
             className="flex flex-col justify-center items-center text-center gap-4">
             <Image
-              src={partner.link}
-              alt={partner.alt}
+              src={partner.image}
+              alt={partner.name}
               width={90}
               height={70}
             />
-            <h2 className="text-lg font-semibold">{partner.alt}</h2>
+            <h2 className="text-lg font-semibold">{partner.name}</h2>
             <p className="max-w-xs -mt-2">{partner.description}</p>
           </div>
         ))}
