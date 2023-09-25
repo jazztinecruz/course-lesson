a2653cedf890293a148108fecc728e17dc19f628
Jazztine Cruz <jzztn.crz@gmail.com>
2023-09-25 14:33:14 +0800

    chore: fix card

--- components/card/index.jsx
+++ components/card/index.jsx
@@ -21,7 +21,7 @@ const Card = ({ data, hasReadMore }) => {
       </div>
       <div className="w-full h-64 lg:h-full relative">
         <Image
-          src={`${BASE_URL}/${data.image}`}
+          src={`${BASE_URL}${data.image}`}
           alt={data.image}
           fill
           objectFit="cover"
