22e6313533506d1972640c7e1d23937759527240
Jazztine Cruz <jzztn.crz@gmail.com>
2023-10-05 11:52:16 +0800

    chore: fix size of hero on mobile

--- components/hero.jsx
+++ components/hero.jsx
@@ -2,7 +2,7 @@ const Hero = ({ title, description }) => {
   return (
     <section className="p-4 relative bg-gradient-to-b from-blue to-pink bg-opacity-50 text-white grid place-items-center text-center py-20">
       <div className="max-w-xl">
-        <h1 className="text-4xl font-bold mb-4">{title}</h1>
+        <h1 className="text-3xl lg:text-4xl font-bold mb-4">{title}</h1>
         <p className="text-lg font-semibold">{description}</p>
       </div>
     </section>
